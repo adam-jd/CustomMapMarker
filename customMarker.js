9374c7523c1738049f97c1d9683281a8fc067417
adam-jd <addeljohn@gmail.com>
2016-09-15 18:44:42 +1000

    Addition of color indicator

--- customMarker.js
+++ customMarker.js
@@ -6,18 +6,21 @@ var customMarker = {
         c.width = 37;
         c.height = 68;
         var img = new Image;
-
+        var context = c.getContext("2d");
         img.src = "images/marker_" + markerdata.markerColor + ".png";
 
         img.onload = function () {
-            c.getContext("2d").drawImage(img, 0, 0);
-            c.getContext("2d").fillStyle = markerdata.markerColor;
-            c.getContext("2d").fillRect(3, 20, 32, 17);
-            c.getContext("2d").stroke();
-            c.getContext("2d").font = "bold 17px arial";
-            c.getContext("2d").fillStyle = 'white';
-            c.getContext("2d").fillText(markerdata.number, 5, 34);
-
+            context.drawImage(img, 0, 0);
+            context.fillStyle = markerdata.markerColor;
+            context.fillRect(3, 20, 32, 17);
+            context.stroke();
+            context.font = "bold 17px arial";
+            context.fillStyle = 'white';
+            context.fillText(markerdata.number, 5, 34);
+            context.fillStyle = markerdata.indicatorColor;
+            context.arc(26, 9, 9, 0, Math.PI * 2, true);
+            context.fill();
+    
             new google.maps.Marker({
                 map: map,
                 icon: c.toDataURL(),
